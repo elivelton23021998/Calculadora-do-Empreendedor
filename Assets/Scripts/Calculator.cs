399bf299d04731d7c5e328e5c82226c1229a36fe
Elivelton Fernando Baia Nascimento <elivelton23021998@gmail.com>
2023-04-18 10:21:10 -0300

    Concluido, falta a ADS e info

--- Assets/Scripts/Calculator.cs
+++ Assets/Scripts/Calculator.cs
@@ -12,13 +12,12 @@ public class Calculator : MonoBehaviour
     float inssEmpregador = 0.2f;
     float fgts = 0.08f;
 
-    [SerializeField] TMP_Text valoresCTPS;
-    [SerializeField] TMP_Text quantoPagar;
+    TMP_Text tabelaAlvo;
     
     void Start() {}
     void Update() {}
 
-    public void CalcularDaCTPS(TMP_InputField valor)
+    public void CalcularDaCTPS(TMP_InputField valor) //pega o valor que ta na carteira e calcula todos os custos mensais
     {
         string valorReal = RemoveNonNumericCharacters(valor.text);
         valorReal = (float.Parse(valorReal) / 100).ToString("N2");
@@ -32,15 +31,15 @@ public class Calculator : MonoBehaviour
         total = string.Format("<b><color=red>R$ {0:N2}</color></b>", float.Parse(total));
         valorReal = string.Format("<b>R$ {0:N2}</b>", float.Parse(valorReal));
 
-        valoresCTPS.text = valorReal + 
+        tabelaAlvo.text = valorReal + 
                            "\n \n \n \n" + "R$ " + inssValor +
-                           "\n \n"       + "R$ " + fgtsValor +
+                           "\n \n \n" + "R$ " + fgtsValor +
                            "\n \n \n"    + "R$ " + feriasValor +
                            "\n \n \n \n" + "R$ " + decimo3 +
                            "\n \n \n"    +  total;
     }
 
-    public void QuantoPagar(TMP_InputField valor)
+    public void QuantoPagar(TMP_InputField valor) // pega o valor em PJ e passa para CLT
     {
         
         string valorReal = RemoveNonNumericCharacters(valor.text);
@@ -53,16 +52,19 @@ public class Calculator : MonoBehaviour
         string feriasValor = ((valorFinal / 12) + ((valorFinal / 3) / 12)).ToString("N2");
         string decimo3 = ((valorFinal / 12)).ToString("N2");
         string total = (float.Parse(valorReal) - float.Parse(inssValor) - float.Parse(fgtsValor) - float.Parse(feriasValor) - float.Parse(decimo3)).ToString("N2");
-        print("total " + total);
         total = string.Format("<b><color=red>R$ {0:N2}</color></b>", float.Parse(total));
         valorReal = string.Format("<b>R$ {0:N2}</b>", float.Parse(valorReal));
 
-        quantoPagar.text = valorReal +
-                           "\n \n \n \n" + "R$ " + inssValor +
-                           "\n \n" + "R$ " + fgtsValor +
-                           "\n \n \n" + "R$ " + feriasValor +
-                           "\n \n \n \n" + "R$ " + decimo3 +
+        tabelaAlvo.text = valorReal +
+                           "\n \n \n \n" + "R$ -" + inssValor +
+                           "\n \n \n" + "R$ -" + fgtsValor +
+                           "\n \n \n" + "R$ -" + feriasValor +
+                           "\n \n \n \n" + "R$ -" + decimo3 +
                            "\n \n \n" + total;
+    }
+    public void TabelaAlvo(TMP_Text tabela)
+    {
+        tabelaAlvo = tabela;
     }
 
     public string RemoveNonNumericCharacters(string input)
